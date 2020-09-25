7efaa4fc223ecb6fa8f65c318d8a1df41cc3637d
Erick Paulino <erick.paulino@outlook.com>
2020-09-25 18:32:39 -0300

    Create ex008.js

---
+++ JavaScript/curso-em-video/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`a velocidade do seu carro é de ${vel}km/h`)
+if (vel > 60) {
+    console.log('você está acima da velocidade permitida, MULTADO!!!')
+}
+console.log('dirija sempre usando cinto de segurança!')
